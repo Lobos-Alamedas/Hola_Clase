ef48967631ca312064d1e5c70e7531340eaaf56b
Lobos-Alamedas <lobos.alamedas2024@gmail.com>
2024-11-20 12:21:39 -0600

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,20 @@
+console.log("Ciclo for");       // Titulo.
+
+for(var i=2;i<=100;i=i+1){      // Incio de for.
+
+    if(i==55){
+
+        console.log("Este numero es 55");
+
+    }
+    if(i==100){
+
+        console.log("Este numero es 100");
+    }
+    else{
+
+        console.log("No es 55 ni 100");
+    }
+
+
+}                               // Fin de for.
